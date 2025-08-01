34c942fde6f16c11042405273fcc20ee07bbd45d
cardinaldeacre <cardinaldeacre@gmail.com>
2025-08-01 14:03:28 +0700

    update zoom materi

--- app/src/main/java/com/example/waristmate/BabDetailScreen.kt
+++ app/src/main/java/com/example/waristmate/BabDetailScreen.kt
@@ -3,47 +3,32 @@ package com.example.waristmate
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.*
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
+import androidx.compose.runtime.*
 import androidx.navigation.NavController
 import com.example.waristmate.data.BabRepository
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.gestures.rememberTransformableState
-import com.example.waristmate.ui.theme.DarkGreen
-import com.example.waristmate.ui.theme.Green
-import com.example.waristmate.ui.theme.Whites
-
-
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import com.example.waristmate.ui.theme.*
 
 @Composable
 fun BottomNavBab(
@@ -89,6 +74,16 @@ fun BabDetailScreen(
     val navbarHeight = 77.dp
     val headerHeight = 70.dp
 
+    var scale by remember { mutableStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero)}
+//    val rotationState = remember { mutableStateOf(0f)}
+
+    val state = rememberTransformableState { zoomChange, offSetChange, rotationChange ->
+        scale *= zoomChange
+        offset += offSetChange
+//        rotationState.value += rotationChange
+    }
+
     if (bab == null) {
         Box(
             modifier = modifier
@@ -120,8 +115,16 @@ fun BabDetailScreen(
                 contentDescription = stringResource(id = bab.titleResId) + " materi",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 8.dp),
-                contentScale = ContentScale.FillWidth
+//                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        translationX = offset.x,
+                        translationY = offset.y,
+//                    rotationZ = rotationState.value
+                    )
+                    .transformable(state),
+                contentScale = ContentScale.Fit
             )
         }
 
